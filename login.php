bb387d57968b43fa36de0a8cf72b12c3ecefed60
stizzle123 <arifayanidowu2@yahoo.com>
2017-11-14 14:01:33 +0100

    New change made on login.php

--- login.php
+++ login.php
@@ -3,5 +3,6 @@
 	
 	echo "<p>This is a Git class Shopping repository text</p>";
 	echo "<p>Making changes on the first file on the master branch</p>";
+	echo "<p>Another change made on login</p>";
 
 ?>
